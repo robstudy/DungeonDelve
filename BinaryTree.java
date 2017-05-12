87f6ef2daadc50dca5df760c12349a6a80088265
Robert Garza <robstudent1988@gmail.com>
2017-05-12 06:31:57 -0700

    implemented return_size, insert, and insert_n functions

--- BinaryTree.java
+++ BinaryTree.java
@@ -38,6 +38,46 @@ public class BinaryTree<T extends Comparable<? super T>> {
 		}
 	}
 
+	private Node root;
+	private int size;
+
+	BinaryTree() {
+		root = null;
+		size = 0;
+	}
+
+	public int return_size() {
+		return size;
+	}
+
+	public void insert(T new_data) {
+		if (root == null) {
+			root = new Node(new_data);
+			size++;
+		} else {
+			insert_n(new_data, root);
+		}
+	}
+
+	private void insert_n(T new_data, Node ptr) {
+		int comparison = ptr.data.compareTo(new_data);
+		if(new_data == ptr.get_data()) return;
+		if(comparison > 0) {
+			if(ptr.get_left() != null) insert_n(new_data, ptr.get_left());
+			else {
+				ptr.set_left(new Node(new_data));
+				size++;
+			}
+		}
+		else if (comparison < 0) {
+			if(ptr.get_right() != null) insert_n(new_data, ptr.get_right());
+			else {
+				ptr.set_right(new Node(new_data));
+				size++;
+			}
+		}
+	}
+
 	public void print() {
 		System.out.println("Placeholder");
 	}
